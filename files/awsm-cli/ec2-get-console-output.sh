32df054d1bedb9e63695d6d966b1eb37e651a690
Stephen Chu <stephen.chu@getbraintree.com>
2016-07-11 23:17:26 +0000

    Added ec2-get-console-output.sh

---
+++ files/awsm-cli/ec2-get-console-output.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/vendor/shflags/src/shflags
+DEFINE_string  'region' '' 'A space-delimited list of region names to filter. e.g. \"us-east-1 us-west-1\"' 'r'
+DEFINE_string  'filter-instance-id' '' 'EC2 instance id' 'i'
+DEFINE_string  'jq' '' 'Output \`jq\` filter' 'j'
+DEFINE_boolean  'log-aws-cli' $FLAGS_FALSE 'Show aws-cli API calls info made' ''
+FLAGS "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
+
+set -euo pipefail
+source $DIR/_common_all.sh
+
+instance_id() {
+  local region="$1"
+  local input="$2"
+  local filters=""
+
+  [ -z "${FLAGS_filter_instance_id:-$(extract "i" $region <<< "$input")}" ] || filters="$filters --instance-id ${FLAGS_filter_instance_id:-$(extract "i" $region <<< "$input" | awk '{ print $1 }')}"
+
+  option_if_not_blank "$filters" "$filters"
+}
+
+output_jq() {
+  jq -r '.Output' | sed 's/\\n/\n/g' | sed 's/\\r/\r/g'
+}
+
+INPUT=$(script_input)
+for region in ${FLAGS_region:-$(extract "region" <<< "$INPUT")}; do
+  aws ec2 --region $region get-console-output $(instance_id $region "$INPUT") \
+    | output_jq
+done
