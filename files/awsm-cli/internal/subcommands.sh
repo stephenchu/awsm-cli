f2480848e644ca7c021354f4c8264dceddc7b5f4
Stephen Chu <stephen.chu@getbraintree.com>
2016-10-20 06:18:55 +0000

    Whitespace

--- files/awsm-cli/internal/subcommands.sh
+++ files/awsm-cli/internal/subcommands.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 AWSCLI_SCRIPTS_DIR="$DIR/.."
+
 printf "_\n"
 find "$AWSCLI_SCRIPTS_DIR" -maxdepth 1 \
                       -type f \
