e7f952fd0fcb53b39db079f14da736d9d88b018a
Stephen Chu <stephen.chu@getbraintree.com>
2017-06-29 00:14:46 +0000

    docoptified ec2-describe-vpcs.sh

--- files/awsm-cli/ec2-describe-vpcs.sh
+++ files/awsm-cli/ec2-describe-vpcs.sh
@@ -1,47 +1,56 @@
 #! /bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $DIR/vendor/shflags/src/shflags
-DEFINE_string  'region' '' 'A space-delimited list of region names to filter. e.g. \"us-east-1 us-west-1\"' 'r'
-DEFINE_string  'filters' '' 'The raw \`--filters\` attribute used with the AWS command' 'f'
-DEFINE_string  'filter-vpc-ids' '' 'VPC Id' 'v'
-DEFINE_string  'jq' '' 'Output \`jq\` filter' 'j'
-DEFINE_string  'output-tags' '' 'Output any additional tags' 't'
-DEFINE_boolean  'log-aws-cli' $FLAGS_FALSE 'Show aws-cli API calls info made' ''
-DEFINE_boolean  'log-jq' $FLAGS_FALSE 'Log jq calls' ''
-FLAGS "$@" || exit $?
-eval set -- "${FLAGS_ARGV}"
-[ $FLAGS_help -eq $FLAGS_FALSE ] || { exit 1; }
 
 set -euo pipefail
 source $DIR/_common_all.sh
+source $(which env_parallel.bash)
 
-filters() {
+eval "$($DIR/vendor/docopts/docopts -h - : "$@" <<EOF
+Usage: ec2-describe-vpcs [-r <region>...] [options]
+
+Options:
+    -r --region=<region>...                AWS region(s) in which the vpc(s) are in [required: as argument or stdin]
+    -i --vpc-id=<vpc-id>...                AWS vpc-id(s) to describe
+    -f --filter=<filter>...                Passed as-is to the \`--filters\` option in awscli command
+    -t --tag=<tag>...                      Any additional tags on the vpc(s) to display in the output
+    --help                                 Show help options
+
+Other Options:
+    --jq=<jq_filter>                       Turns tabular output into JSON output, with a JQ filter already applied
+    --log-awscli                           Logs every awscli command line runs to stderr [default: false]
+    --log-jq                               Logs every jq command runs to stderr          [default: false]
+EOF
+)"
+
+aws:filters() {
   local region="$1"
   local input="$2"
-  local filters=""
+  local filter_values=""
 
-  [ -z "${FLAGS_filters}" ]                                               || filters="$filters $FLAGS_filters"
-  [ -z "${FLAGS_filter_vpc_ids:-$(extract "vpc" $region <<< "$input")}" ] || filters="$filters Name=vpc-id,Values=$(string.join "," <<< "${FLAGS_filter_vpc_ids:-$(extract "vpc" $region <<< "$input")}")"
+  local vpc_ids="${vpc_id[@]:-$(stdin:extract "vpc" $region <<< "$input")}"
 
-  echo_if_not_blank "$filters" "--filters ${filters}"
+  [ -z "$(string.join " " <<< "${filter[@]}")" ] || filter_values="$filter_values $(string.join " " <<< "${filter[@]}")"
+  [ -z "$(string.join "," <<< "$vpc_ids")" ]     || filter_values="$filter_values Name=vpc-id,Values=$(string.join "," <<< "$vpc_ids")"
+
+  echo_if_not_blank "$filter_values" "--filters ${filter_values}"
 }
 
-output_jq() {
+output:jq() {
   local region="$1"
   local default=$(cat <<EOS
     [
       \$region,
       .VpcId,
       (.Tags | tag_value("Name")) // "n/a",
-      $(output.tags "$FLAGS_output_tags")
+      $(output.tags "${tag[@]}")
     ] | join("\t")
 EOS
   )
 
-  jq -L $DIR/jq -L $DIR/jq -r --arg region $region "include \"aws\"; .Vpcs[] | ${FLAGS_jq:-$default}"
+  jq -L $DIR/jq -r --arg region $region "include \"aws\"; .Vpcs[] | ${jq:-$default}"
 }
 
-INPUT=$(script_input_with_region)
-headers "Region VpcId $(headers.tag "Name") $(headers.tags "$FLAGS_output_tags")"
-env_parallel -k 'aws ec2 --region {} describe-vpcs $(filters {} "$INPUT") | output_jq {}' ::: ${FLAGS_region:-$(extract "region" <<< "$INPUT")}
+INPUT="$(stdin:aws-regional-input)"
+output:headers "Region VpcId $(headers.tag "Name") $(headers.tags "${tag[@]:-}")"
+env_parallel -k 'aws ec2 --region {} describe-vpcs $(aws:filters {} "$INPUT") | output:jq {}' ::: ${region[@]:-$(stdin:extract "region" <<< "$INPUT")}
