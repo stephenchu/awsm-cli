8a6e53b42b4b2854f17b1283f11b6a89e9a27e20
Stephen Chu <stephen.chu@getbraintree.com>
2017-06-29 00:59:12 +0000

    docoptified ec2-describe-subnets.sh

--- files/awsm-cli/ec2-describe-subnets.sh
+++ files/awsm-cli/ec2-describe-subnets.sh
@@ -1,35 +1,45 @@
 #! /bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $DIR/vendor/shflags/src/shflags
-DEFINE_string   'region' '' 'AWS region(s)' 'r'
-DEFINE_string   'filters' '' 'The raw \`--filters\` attribute used with the AWS command' 'f'
-DEFINE_string  'filter-vpc-id' '' 'VPC Id' 'v'
-DEFINE_string  'subnet-ids' '' 'Subnet Id' 's'
-DEFINE_string   'jq' '' 'Output \`jq\` filter' 'j'
-DEFINE_string   'output-tags' '' 'Output any additional tags' 't'
-DEFINE_boolean  'log-aws-cli' $FLAGS_FALSE 'Show aws-cli API calls info made' ''
-DEFINE_boolean  'log-jq' $FLAGS_FALSE 'Log jq calls' ''
-FLAGS "$@" || exit $?
-eval set -- "${FLAGS_ARGV}"
-[ $FLAGS_help -eq $FLAGS_FALSE ] || { exit 1; }
 
 set -euo pipefail
 source $DIR/_common_all.sh
+source $(which env_parallel.bash)
 
-filters() {
+eval "$($DIR/vendor/docopts/docopts -h - : "$@" <<EOF
+Usage: ec2-describe-subnets [-r <region>...] [options]
+
+Options:
+    -r --region=<region>...                AWS region(s) in which the subnet(s) are in [required: as argument or stdin]
+    -i --subnet-id=<subnet-id>...          AWS subnet-id(s) to describe
+    -v --vpc-id=<vpc-id>...                AWS vpc-id(s) for the subnet(s)
+    -f --filter=<filter>...                Passed as-is to the \`--filters\` option in awscli command
+    -t --tag=<tag>...                      Any additional resource tags to display in the output
+    --help                                 Show help options
+
+Other Options:
+    --jq=<jq_filter>                       Turns tabular output into JSON output, with a JQ filter already applied
+    --log-awscli                           Logs every awscli command line runs to stderr [default: false]
+    --log-jq                               Logs every jq command runs to stderr          [default: false]
+EOF
+)"
+
+aws:filters() {
   local region="$1"
   local input="$2"
-  local filters=""
+  local filter_values=""
 
-  [ -z "${FLAGS_filters}" ]                                              || filters="$filters $FLAGS_filters"
-  [ -z "${FLAGS_filter_vpc_id:-$(extract "vpc" $region <<< "$input")}" ] || filters="$filters Name=vpc-id,Values=$(echo -n "${FLAGS_filter_vpc_id:-$(extract "vpc" $region <<< "$input")}" | string.join ",")"
-  [ -z "${FLAGS_filter_subnet_ids:-$(extract "subnet" $region <<< "$input")}" ] || filters="$filters Name=subnet-id,Values=$(echo -n "${FLAGS_filter_subnet_ids:-$(extract "subnet" $region <<< "$input")}" | string.join ",")"
+  local subnet_ids="${subnet_id[@]:-$(stdin:extract "subnet" $region <<< "$input")}"
+  local vpc_ids="${vpc_id[@]:-$(stdin:extract "vpc" $region <<< "$input")}"
 
-  echo_if_not_blank "$filters" "--filters ${filters}"
+  [ -z "$(string.join " " <<< "${filter[@]}")" ] || filter_values="$filter_values $(string.join " " <<< "${filter[@]}")"
+  [ -z "$(string.join "," <<< "$subnet_ids")" ]  || filter_values="$filter_values Name=subnet-id,Values=$(string.join "," <<< "$subnet_ids")"
+  [ -z "$(string.join "," <<< "$vpc_ids")" ]     || filter_values="$filter_values Name=vpc-id,Values=$(string.join "," <<< "$vpc_ids")"
+
+  echo_if_not_blank "$filter_values" "--filters ${filter_values}"
 }
 
-output_jq() {
+output:jq() {
   local region="$1"
   local default=$(cat <<EOS
     [
@@ -40,13 +50,13 @@ output_jq() {
       .CidrBlock,
       .State,
       $(output.tag "Name"),
-      $(output.tags "$FLAGS_output_tags")
+      $(output.tags "${tag[@]}")
     ] | join("\t")
 EOS
   )
-  jq -L $DIR/jq -r --arg region $region "include \"aws\"; .Subnets[] | ${FLAGS_jq:-$default}"
+  jq -L $DIR/jq -r --arg region $region "include \"aws\"; .Subnets[] | ${jq:-$default}"
 }
 
-INPUT=$(script_input_with_region)
-headers "Region VpcId AvailabilityZone SubnetId CidrBlock State $(headers.tag "Name") $(headers.tags "$FLAGS_output_tags")"
-env_parallel -k 'aws ec2 --region {} describe-subnets $(filters {} "$INPUT") | output_jq {}' ::: ${FLAGS_region:-$(extract "region" <<< "$INPUT")}
+INPUT="$(stdin:aws-regional-input)"
+output:headers "Region VpcId AvailabilityZone SubnetId CidrBlock State $(headers.tag "Name") $(headers.tags "${tag[@]:-}")"
+env_parallel -k 'aws ec2 --region {} describe-subnets $(aws:filters {} "$INPUT") | output:jq {}' ::: ${region[@]:-$(stdin:extract "region" <<< "$INPUT")}
